779c467aafb0b336afc0577464968fd9a0401bf7
JaimeSerrano15 <serranojaime41@gmail.com>
2024-09-07 14:27:45 -0600

    Adding Default Layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="flex flex-col p-8">
     <div>
-      <input type="text" class="rounded border-2 border-gray-400 w-full" placeholder="Search for Meals">
+      
     </div>
 
     <div class="flex justify-center gap-2 mt-2">
       <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
         {{ letter }}
       </router-link>
-  </div>
+    </div>
   </div>
 </template>
 
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,9 +1,24 @@
 <template>
+  <div class="p-8">
+    <input type="text" class="rounded border-2 border-gray-400 w-full" placeholder="Search for Meals" v-model="keyword" @change="searchMeals">
+  </div>
+
   <div>
-    Search by Name
+    <pre>{{ meals }}</pre>
   </div>
 </template>
 
 <script setup>
+  import store from '@/store';
+  import { computed, ref } from 'vue';
   
+  const keyword = ref('');
+  const meals = computed(() => store.state.searchedMeals);
+
+  function searchMeals() {
+    console.log(keyword.value);
+    
+    store.dispatch('searchMeals', keyword.value);
+  }
+
 </script>
